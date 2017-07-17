19d8dd47649af530867ef27017453c0a24f2c6b6
Anatolijus <join.eu.pn@gmail.com>
2017-07-17 18:47:19 +0300

    stack constructor tests created before implementation

--- src/SimpleStack.java
+++ src/SimpleStack.java
@@ -3,6 +3,14 @@
  */
 public class SimpleStack {
 
+    int stackSize;
+    int[] stackArray;
+    int top;
 
+    public SimpleStack(int stackSize) {
+        this.stackSize = 0;
+        this.stackArray = new int[0];
+        this.top = 0;
+    }
 
 }
--- test/SimpleStackTest.java
+++ test/SimpleStackTest.java
@@ -1,8 +1,22 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 /**
  * Created by Anatolijus on 2017-07-17.
  */
 public class SimpleStackTest {
 
+    SimpleStack stack = new SimpleStack(25);
 
+    @Test public void stackHasMaximimSize() {
+        assertEquals(25, stack.stackSize);
+    }
+
+    @Test public void stackHasArrayWithMaximimSize() {
+        assertEquals(25, stack.stackArray.length);
+    }
+
+    @Test public void stackTopPointedToTopElenentInArray() {
+        assertEquals(-1, stack.top);
+    }
 
 }
